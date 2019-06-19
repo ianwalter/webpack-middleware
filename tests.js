e6ee335fa2777142fcf4f7da55a1492f3b29d65e
Ian Walter <public@iankwalter.com>
2019-06-19 13:36:26 -0400

    Working on test

--- tests.js
+++ tests.js
@@ -1,5 +1,9 @@
 const { test } = require('@ianwalter/bff')
+const createTestServer = require('@ianwalter/test-server')
+const c2k = require('koa-connect')
+const webpackMiddleware = require('.')
 
-test.skip('middleware', () => {
-
+test('middleware', async () => {
+  const server = await createTestServer()
+  server.use(c2k(webpackMiddleware))
 })
